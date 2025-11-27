63bf0f85af7a098b2edb54a86bce773befb9a062
ewwerson1 <ewwersondev@gmail.com>
2025-11-27 00:09:28 -0300

    Atualizando Cliente

--- src/models/Client.js
+++ src/models/Client.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+ const mongoose = require("mongoose");
 
 const clientSchema = new mongoose.Schema(
   {
@@ -6,14 +6,57 @@ const clientSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     telefone: { type: String, required: true },
     cpf: { type: String, required: true, unique: true },
+
+    // URL do arquivo PDF salvo com Multer
+    contrato: {
+      type: String,
+      default: null,
+    },
+
+    // Data da validade da CNH
+    validadeCnh: {
+      type: Date,
+      required: true,
+    },
+
+    // Status automático baseado na validade da CNH
+    validadeStatus: {
+      type: String,
+      enum: ["ok", "atencao", "vencida"],
+      default: "ok",
+    },
+
     alugueis: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Aluguel",
+        ref: "rents",
       },
     ],
   },
   { timestamps: true }
 );
+
+// Atualiza o status da CNH baseado na data
+clientSchema.pre("save", function (next) {
+  if (!this.validadeCnh) {
+    this.validadeStatus = "ok";
+    return next();
+  }
+
+  const hoje = new Date();
+  const validade = new Date(this.validadeCnh);
+
+  const diferencaDias = Math.ceil((validade - hoje) / (1000 * 60 * 60 * 24));
+
+  if (diferencaDias < 0) {
+    this.validadeStatus = "vencida";
+  } else if (diferencaDias <= 30) {
+    this.validadeStatus = "atencao";
+  } else {
+    this.validadeStatus = "ok";
+  }
+
+  next();
+});
 
 module.exports = mongoose.model("Client", clientSchema);
